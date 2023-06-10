4e1c0c805903e6b4cf8af3d620055ea86fc67187
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-10 16:56:41 -0300

    Update atividadeA.c

--- atividadeA.c
+++ atividadeA.c
@@ -1,6 +1,5 @@
 // Efetuar a leitura de dois valores numéricos inteiros representados pelas variáveis A e B e apresentar o resultado da diferença do maior valor pelo menor valor.
 
-#include <stdio.h> //Biblioteca necessária para entrada e saída de dados.
 #include <stdlib.h> //Biblioteca necessária para a função "system".
 #include <locale.h> //Biblioteca necessária para configurar a localização padrão.
 
@@ -21,11 +20,13 @@ int main() //Ponto de entrada do programa.
     */
     system("cls"); //A função "system" é chamada com o argumento "cls". Isso irá limpar a tela do console.
 
-    printf("Digite o valor de A: "); //É exibido a mensagem solicitando ao usuário que digite o valor de A usando a função "printf".
+printf("\n================================\n");
+    printf("    Digite o valor de A: "); //É exibido a mensagem solicitando ao usuário que digite o valor de A usando a função "printf".
     scanf("%d", &A); //O valor digitado pelo usuário é lido usando a função "scanf" e armazenado na variável A.
 
-    printf("Digite o valor de B: "); //É exibido a mensagem solicitando ao usuário que digite o valor de B usando a função "printf".
+    printf("    Digite o valor de B: "); //É exibido a mensagem solicitando ao usuário que digite o valor de B usando a função "printf".
     scanf("%d", &B); //O valor digitado pelo usuário é lido usando a função "scanf" e armazenado na variável B.
+printf("================================\n");
 
     if (A > B) {
         diferenca = A - B;
@@ -36,18 +37,32 @@ int main() //Ponto de entrada do programa.
     O programa verifica se A é maior que B usando a estrutura de controle "if". Se a condição for verdadeira, a diferença entre A e B é
     calculada e atribuída à variável "diferenca". Caso contrário, a diferença entre B e A é calculada e atribuída à variável "diferenca".
     */
-    printf("A diferença entre o maior e o menor valor é: %d\n", diferenca); //A diferença calculada é exibida na tela usando a função "printf".
+    for (int i = 0; i < 4; i++) { //Imprime uma nova linha no console quatro vezes.
+        printf("\n");
+    }
+printf("=========================================================\n");
+    printf("    A diferença entre o maior e o menor valor é: [%d]\n", diferenca); //A diferença calculada é exibida na tela usando a função "printf".
+printf("=========================================================\n");
 
+    for (int i = 0; i < 10; i++) { //Imprime uma nova linha no console dez vezes.
+        printf("\n");
+    }
+
+printf("///////////////////////\n");
+printf("//////bY: Paulo////////\n"); //Assinatura.
+printf("///////////////////////\n\n");
     system("pause");
     /*
     A função "system" é chamada novamente com o argumento "pause". Isso faz com que o programa
     aguarde a entrada do usuário antes de prosseguir.
     */
+
     }while(A !=0 && B !=0);
     /*
     O bloco "do-while" é encerrado com a condição "(A != 0 && B != 0)". Isso significa que o loop
     continuará enquanto ambos A e B forem diferentes de zero.
     */
+
 return 0;
 /*
 O programa retorna 0, indicando que foi concluído com sucesso. O programa continua repetindo esse processo
